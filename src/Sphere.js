6ed309c7c007bc99b9edbac04ef8f2ba45fc0cdf
Ben Junya <bjunya@gmail.com>
2020-01-12 18:55:24 -0800

    Start with scaleform audio vis

--- src/Sphere.js
+++ src/Sphere.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useRafLoop } from 'react-use';
+import { css } from 'emotion';
+
+import { ReactAudioContext } from './AudioProvider';
 
 function Sphere() {
-  return <div>SPHERE</div>;
+  const audioContext = useContext(ReactAudioContext);
+  const [ values, setValues ] = useState([]);
+
+  useRafLoop( () => {
+    const { analyzer } = audioContext;
+    const bufferLength = analyzer.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    analyzer.getByteTimeDomainData(dataArray);
+    setValues(dataArray);
+
+  });
+
+  const style = css`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    .sphere {
+      position: absolute;
+      height: 300px;
+      width: 300px;
+    }
+
+  `;
+  return (
+    <div className={style}>
+      <div className="sphere-container">
+        {Array.from(values).map( (val, idx) => {
+          const g = Math.floor(val / 255);
+          console.log(g);
+
+          return (
+            <div
+              key={idx}
+              className="sphere"
+              style={{
+                backgroundColor: `rgba(255, ${g}, ${g}, 1)`,
+                transform: `scale(${val / 255})`
+              }}
+
+            />
+          )
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Sphere;
