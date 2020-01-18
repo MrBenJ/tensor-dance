497219f17798419f9cbc6d428bfa279e58f66a44
Ben Junya <bjunya@gmail.com>
2020-01-17 17:51:57 -0800

    Make the wall super awesome

--- src/Sphere.js
+++ src/Sphere.js
@@ -5,53 +5,27 @@ import { css } from 'emotion';
 import { ReactAudioContext } from './AudioProvider';
 
 function Sphere() {
-  const audioContext = useContext(ReactAudioContext);
-  const [ values, setValues ] = useState([]);
+  // const audioContext = useContext(ReactAudioContext);
+  // const [ values, setValues ] = useState([]);
 
-  useRafLoop( () => {
-    const { analyzer } = audioContext;
-    const bufferLength = analyzer.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-    analyzer.getByteTimeDomainData(dataArray);
-    setValues(dataArray);
+  // useRafLoop( () => {
+  //   // const { analyzer } = audioContext;
+  //   // const bufferLength = analyzer.frequencyBinCount;
+  //   // const dataArray = new Uint8Array(bufferLength);
+  //   // analyzer.getByteTimeDomainData(dataArray);
+  //   // analyzer.getByteFrequencyData(dataArray);
+  //   // setValues(dataArray);
 
-  });
+  // });
 
-  const style = css`
-    display: flex;
-    height: 100vh;
-    width: 100vw;
-    justify-content: center;
-    align-items: center;
-    position: relative;
+  // const style = css`
 
-    .sphere {
-      position: absolute;
-      height: 300px;
-      width: 300px;
-    }
+  // `;
 
-  `;
+  // console.log(values);
   return (
-    <div className={style}>
-      <div className="sphere-container">
-        {Array.from(values).map( (val, idx) => {
-          const g = Math.floor(val / 255);
-          console.log(g);
-
-          return (
-            <div
-              key={idx}
-              className="sphere"
-              style={{
-                backgroundColor: `rgba(255, ${g}, ${g}, 1)`,
-                transform: `scale(${val / 255})`
-              }}
-
-            />
-          )
-        })}
-      </div>
+    <div className={"style"}>
+      SPHERE
     </div>
   );
 }
--- src/Wall.js
+++ src/Wall.js
@@ -1,64 +1,106 @@
 import React, { useContext, useState } from 'react';
-import { useRafLoop } from 'react-use';
+import { useRafLoop, useKey } from 'react-use';
 import { css } from 'emotion';
 
 import { ReactAudioContext } from './AudioProvider';
 
+const COLORS = [ 'b', 'g', 'r' ];
+const CLIPS = [
+  // FULL SQUARE
+  'polygon(69% 0, 100% 0, 100% 47%, 100% 100%, 54% 100%, 0 100%, 0 0, 29% 0)',
+
+  // HEART
+  'polygon(50% 25%, 75% 0, 100% 25%, 50% 100%, 50% 100%, 50% 100%, 0 25%, 25% 0)',
+
+  // WEIRD KEYHOLE THING
+  'polygon(50% 0, 100% 25%, 75% 50%, 100% 100%, 50% 75%, 0 100%, 25% 50%, 0 25%)'
+
+];
 function Wall() {
   const audioContext = useContext(ReactAudioContext);
-  const [ wave, setWave ] = useState([]);
+  const [ values, setValues ] = useState([]);
+  const [ color, setColor ] = useState(0);
+  const [ clip, setClip ] = useState(0);
 
   useRafLoop(() => {
     const { analyzer } = audioContext;
     const bufferLength = analyzer.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
-    analyzer.getByteTimeDomainData(dataArray);
-    setWave(dataArray);
+    analyzer.getByteFrequencyData(dataArray);
+    setValues(dataArray);
   });
 
+  // ON SPACEBAR PRESS
+  useKey(' ', () => {
+    if (color + 1 === COLORS.length) {
+      setColor(0);
+      return;
+    }
+    setColor(color + 1);
+  }, undefined, [ color ]);
+
+  useKey('s', () => {
+    if (clip + 1 === CLIPS.length) {
+      setClip(0);
+      return;
+    }
+    setClip(clip + 1);
+  }, undefined, [ clip ]);
+
+  useKey('b', () => {
+
+  }, undefined, []);
+
   const style = css`
+    display: flex;
     height: 100vh;
     width: 100vw;
-    display: flex;
-    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
 
-    .col {
-      display: flex;
+    .wall-container {
+      height: 100%;
+      width: 100%;
+      position: relative;
     }
-    .box {
-      flex: 1;
-      height: 12.5vh;
-      width: 12.5vw;
-
-      animation: circle .4s infinite alternate;
-
-      &:hover {
-
-        pointer: cursor;
-      }
-    }
-
-    @keyframes circle {
-      0% {
-        border-radius: 0;
-      }
-      100% {
-        border-radius: 100%;
-      }
+    .wall-piece {
+      height: 100vh;
+      width: 100vw;
+      clip-path: ${CLIPS[clip]};
+      transition: clip-path 2s ease;
+      position: absolute;
     }
   `;
 
   return (
     <div className={style}>
-      {Array(8).fill('x').map( (_, idx) => {
-        return (
-          <div key={idx} className="col">
-            {Array(8).fill('x').map( (__, _idx) => {
-              return <div key={`${idx}-${_idx}`} className="box" style={{ backgroundColor: `hsl(${wave[idx + _idx]}, 50%, 50%)`}}/>;
-            })}
-          </div>
-        );
-      })}
+      <div className="wall-container">
+        {Array.from(values).map( (val, idx) => {
+          const r = COLORS[color] === 'r'
+            ? Math.floor(val % 255)
+            : 0
+
+          const g = COLORS[color] === 'g'
+            ? Math.floor(val % 255)
+            : 0
+
+            const b = COLORS[color] === 'b'
+            ? Math.floor(val % 255)
+            : 0
+
+          return (
+            <div
+              key={idx}
+              className="wall-piece"
+              style={{
+                backgroundColor: `rgba(${r}, ${g}, ${b}, ${idx / values.length})`,
+                transform: `scale(${val / 255})`
+              }}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
